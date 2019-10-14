3057a75bd65b0f6a2d71c95948372e97714b8bfd
liudanqing <1278476591@qq.com>
2019-10-14 15:40:24 +0800

    add

---
+++ precache-manifest.1375faf438c368898e993dcbff2c431d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7601aa715566fafb6270bc9eb8039d59",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9cb535d53a62959e22e4",
+    "url": "/static/css/main.444c874e.chunk.css"
+  },
+  {
+    "revision": "fa06955aa8ed173b4531",
+    "url": "/static/js/2.f05fc8da.chunk.js"
+  },
+  {
+    "revision": "9cb535d53a62959e22e4",
+    "url": "/static/js/main.9770b562.chunk.js"
+  },
+  {
+    "revision": "628ea25a63cbc5b89a27",
+    "url": "/static/js/runtime-main.9f147cbc.js"
+  }
+]);
